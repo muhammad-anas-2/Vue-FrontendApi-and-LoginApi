66afbfa37fbcaa7b34c3f91ac6ea33722716a8a0
Al-hafiz Academy App <itsranas517@gmail.com>
2021-09-17 17:16:26 +0500

    Add registraton page

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,25 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/Login">Login</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    
+  }
+}
+</script>
 
 <style>
 #app {
